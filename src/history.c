7d8eb0fe696ac148293acb85881630bfb2fc12ae
Victor Cabral <vcabral001@gmail.com>
2021-03-18 14:04:49 -0600

    Deleted history.c code files, code incomplete or incorrect

--- src/history.c
+++ src/history.c
@@ -1,13 +1,7 @@
 #ifndef _HISTORY_
+
 #define _HISTORY_
 
-#include "tokenizer.h"
-#include "history.h"
-#include <string.h>
-
-#include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 
 typedef struct s_Item {
@@ -32,16 +26,9 @@ typedef struct s_List {
 
 /* Initialize the linked list to keep the history. */
 
-List* init_history()
-{
-  
-  List* list;
-
-  list = malloc(sizeof(List));
-
-  list->root = NULL;
-
-  return list; 
+List* init_history(){
+  list.size = size;
+  list.data = data;
 }
 
 
@@ -54,85 +41,29 @@ List* init_history()
 
 */
 
-void add_history(List *list, char *str)
-  {
-
-    // First check if our list is empty, if so allocate memory for root and them set root->str = str
-
-    if(list->root == NULL)
-
-      {
-
-	list->root = malloc(sizeof(Item));
-
-	list->root->str = malloc(sizeof(char)*strlen(str)+1);
-
-	int n = 0;
-
-	while(str[n])
-
-	  {
-
-	    list->root->str[n] = str[n];
-
-	    n++;
-
-	  }
-
-	list->root->id = 1;
-
-	return;
-
-      }
-
-    // Create iterator to loop through our list
-
-    Item* iterator = list->root;
-
-    int i = 1;
-
-    // As long as our next item is valid keep moving through the list
-
-    while (iterator->next)
-
-      {
-
-	i++;
-
-	iterator = iterator->next;
-
-      }
-
-    // Once our loop finds an end point in our list allocate memory for next item and move to our next item
-
-    iterator->next = malloc(sizeof(Item));
-
-    iterator = iterator->next;
-
-    // Set values to our new item in the list
-
-    iterator->str = malloc(sizeof(char)*strlen(str)+1);
-
-    int j = 0;
-
-    while(str[j])
-
-      {
-
-	iterator->str[j] = str[j];
-
-	j++;
-
-      }
-
-    iterator->id = i+1;
-
-    iterator->next = NULL;
-
-  }
+void add_history(List *list, char *str){
   
+  int size = arraylist_get_size(*list);
 
-  
+  char *new_data;
+
+
+
+  new_data = realloc(list.data, (size + 1) * sizeof new_data[0]);
+
+
+
+  if (new_data)
+
+    {
+
+      new_data[size] = value;
+
+      arraylist_set_data_collection(list, new_data);
+
+      ++list.size;
+
+    }
 
 
 /* Retrieve the string stored in the node where Item->id == id.
@@ -141,82 +72,23 @@ void add_history(List *list, char *str)
 
    int id - the id of the Item to find */
 
-  char *get_history(List *list, int id)
-  {
+char *get_history(List *list, int id);
 
-    Item* iterator = list->root;
+   return id;
 
-    while(iterator)
 
-      {
+/ *Print the entire contents of the list. */
 
-	if (iterator->id == id)
-
-	  {
-
-	    return iterator->str;
-
-	  }
-
-	iterator = iterator->next;
-
-      }
-
-    return NULL;
-
+void print_history(List *list){
+  for(int i=0; i< list;i++){
+    printf(%c, list[i]);
   }
-
- 
-
-
-
-/*Print the entire contents of the list. */
-void print_history(List *list)
-{
-
-       Item* iterator = list->root;
-
-       while(iterator)
-
-	 {
-
-	   printf("%d: ", iterator->id);
-
-	   printf("%s", iterator->str);
-
-	   iterator = iterator->next;
-
-	 }
-
  }
- 
 
-/*Free the history list and the strings it references. */
+/ *Free the history list and the strings it references. */
 
-void free_history(List *list)
-   {
+void free_history(List *list);
 
-     Item* iterator = list->root;
-
-     Item* temp;
-
-     while(iterator)
-
-       {
-
-	 temp = iterator;
-
-	 iterator = iterator->next;
-
-	 free(temp->str);
-
-	 free(temp);
-
-       }
-
-     // free(list);
-
-   }
 
 
 #endif
